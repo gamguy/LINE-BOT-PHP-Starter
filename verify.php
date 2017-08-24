6497a2008d598be6f965be831bcca5200cde016a
gamguy <guy_gam@hotmail.com>
2017-08-24 13:54:55 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,9 @@
-<?php
+.<?php
 $access_token = 'N289D+HEfDtM1cJTXeDU1+Rd1xwiRyJZdS+KyG0SJOokrjHXx4j+DSVQs0FVtOq3Fp0Y211dS999I+V4bl45piUIvTHqvdZgR8ORaMaJgSBV5A0prPtNxHW+M3G4HzPzPlr/9FsrM8FuOwJRDVPwCwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' N289D+HEfDtM1cJTXeDU1+Rd1xwiRyJZdS+KyG0SJOokrjHXx4j+DSVQs0FVtOq3Fp0Y211dS999I+V4bl45piUIvTHqvdZgR8ORaMaJgSBV5A0prPtNxHW+M3G4HzPzPlr/9FsrM8FuOwJRDVPwCwdB04t89/1O/w1cDnyilFU=);
+$headers = array('Authorization: Bearer ' .N289D+HEfDtM1cJTXeDU1+Rd1xwiRyJZdS+KyG0SJOokrjHXx4j+DSVQs0FVtOq3Fp0Y211dS999I+V4bl45piUIvTHqvdZgR8ORaMaJgSBV5A0prPtNxHW+M3G4HzPzPlr/9FsrM8FuOwJRDVPwCwdB04t89/1O/w1cDnyilFU=);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
